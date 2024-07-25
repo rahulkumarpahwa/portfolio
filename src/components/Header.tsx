f2cfacd2449952e7da3195c04f2baf2e4c51ac48
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-25 17:10:42 +0530

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 "use client";
-
 import DarkModeToggle from "./DarkModeToggle";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-between bg-[#51abb2] p-8 shadow-xl dark:bg-black">
+      <div className="w-20">
+        <Image src="/logo.png" alt="Logo" width={100} height={100} className="w-full" />
+      </div>
       <DarkModeToggle />
     </div>
   );
