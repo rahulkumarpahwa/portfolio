807efb05009e58c960431d030aa4fe9333907a9e
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-05 23:15:44 +0530

    created footer and some style

---
+++ src/components/Footer.tsx
@@ -0,0 +1,106 @@
+"use client";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#51abb2] text-white py-12">
+      <div
+        className="max-w-6xl  mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 px-4 sm:px-6 lg:px-8 
+      text-center sm:text-left lg:text-left   "
+      >
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
+          <p className="mb-4 ">
+            <span className="flex items-center gap-2 py-1">
+              <Image
+                src="/favicon.ico"
+                alt="Vercel Logo"
+                className="dark:invert"
+                width={30}
+                height={24}
+                priority
+              />{" "}
+              <span> Portfolio</span>{" "}
+            </span>
+            {/* <span>
+              authentication app which was build for the learning authentication
+              with nextjs.
+            </span> */}
+          </p>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
+          <ul>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Login
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Signup
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
+          <div className="flex flex-col">
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Facebook
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Twitter
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Instagram
+            </a>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
+          <p>New Delhi, India</p>
+          <p>Delhi 10001</p>
+          <p>Email: info@authapp.com</p>
+          <p>Phone: (123) 456-7890</p>
+        </div>
+      </div>
+      <p className="text-center text-xs pt-8">
+        © 2024 Portfolio App. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
