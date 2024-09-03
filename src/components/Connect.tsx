27e6c38ddb49b85ddf2b51427970b985e81d2ff5
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-09-03 07:20:02 +0530

    created github cards

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,7 +1,6 @@
 "use client";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import Link from "next/link";
+import GithubCard from "./GithubCard";
 
 const Connect = () => {
   const [data, setData] = useState({
@@ -22,33 +21,14 @@ const Connect = () => {
     // console.log(json);
     setData(json);
   };
+
+
   return (
     data != undefined && (
-      <div className="flex items-center justify-center gap-10">
-        <div className="border border-gray-500 hover:border-white max-w-[17rem] rounded-lg">
-          <Link href={data?.html_url ? data.html_url : "none"}>
-            <Image
-              src={data?.avatar_url}
-              height="1000"
-              width="1000"
-              className="h-60 w-full object-cover rounded-tl-lg rounded-tr-lg"
-              alt={data?.name}
-            />
-            <div className="p-2">
-              <h2 className="text-xl text-center text-bold ">{data.name}</h2>
-              <p className="text-gray-500 hover:text-white">{data.bio}</p>
-              <div className="text-gray-500 hover:text-white">
-                X :{" "}
-                <Link
-                  href={`https://twitter.com/${data.twitter_username}`}
-                  target="_blank"
-                >
-                  @rahulkumarpahwa
-                </Link>
-              </div>
-            </div>
-          </Link>
-        </div>
+      <div className="flex items-center justify-center gap-10 m-8 p-8">
+        <GithubCard data={data} />
+        <GithubCard data={data} />
+        <GithubCard data={data} />
       </div>
     )
   );
--- src/components/GithubCard.tsx
+++ src/components/GithubCard.tsx
@@ -1,2 +1,44 @@
 "use client";
+import Image from "next/image";
+import Link from "next/link";
 
+interface DATA {
+  data: {
+    html_url: string;
+    avatar_url: string;
+    name: string;
+    bio: string;
+    twitter_username: string;
+  };
+}
+
+const GithubCard = ({ data }: DATA) => {
+  return (
+    <div className="border border-gray-500 hover:border-white max-w-[17rem] rounded-lg">
+      <Link href={data?.html_url ? data?.html_url : "none"}>
+        <Image
+          src={data?.avatar_url}
+          height="1000"
+          width="1000"
+          className="h-60 w-full object-cover rounded-tl-lg rounded-tr-lg"
+          alt={data?.name}
+        />
+        <div className="p-2">
+          <h2 className="text-xl text-center text-bold ">{data.name}</h2>
+          <p className="text-gray-500 hover:text-white">{data.bio}</p>
+          <div className="text-gray-500 hover:text-white">
+            X :{" "}
+            <Link
+              href={`https://twitter.com/${data.twitter_username}`}
+              target="_blank"
+            >
+              @rahulkumarpahwa
+            </Link>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default GithubCard;
