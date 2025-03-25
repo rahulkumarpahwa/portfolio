a6b2ce26013d9a0c907d73f03b450394144c14ab
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-03-25 15:52:58 +0530

    added gototop button, navlinks to sections and update packages and updated timeline details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Top from "@/components/Top";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Top/>
         <Footer />
       </body>
     </html>
