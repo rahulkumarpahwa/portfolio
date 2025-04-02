412b6dde8e6730cb43515c9c3a046850405ae975
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-04-03 02:06:18 +0530

    feat: enhance journey section with links to companies and update images

--- src/components/Journey/Data.tsx
+++ src/components/Journey/Data.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 const Data = () => {
   return [
@@ -8,28 +9,26 @@ const Data = () => {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-normal mb-8">
-            Worked as a Software Engineer Intern at Salesine, contributing to the
-            development of web applications using modern technologies. Designed
-            and implemented scalable solutions, optimized front-end performance,
-            and collaborated with cross-functional teams to improve user
-            experience.
+            Worked as a Software Engineer Intern at{" "}
+            <Link
+              href="https://salesine.com"
+              className="font-bold hover:underline"
+            >
+              Salesine
+            </Link>
+            , contributing to the development of web applications using modern
+            technologies. Designed and implemented scalable solutions, optimized
+            front-end performance, and collaborated with cross-functional teams
+            to improve user experience.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/templates/startup-1.webp"
+              src="https://res.cloudinary.com/dwtcjjxwc/image/upload/v1743588056/portfolio/salesine_lyp3tz.png"
               alt="startup template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-            <Image
-              src="https://assets.aceternity.com/templates/startup-2.webp"
-              alt="startup template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-           
           </div>
         </div>
       ),
@@ -39,36 +38,21 @@ const Data = () => {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-normal mb-8">
-            Gained experience as a Web Developer and Designer at The Sparks
-            Foundation, focusing on UI/UX and front-end development. Built
-            interactive web pages, enhanced accessibility, and improved overall
-            design responsiveness for better user engagement.
+            Gained experience as a Web Developer and Designer at{" "}
+            <Link
+              href="https://thesparksfoundationsingapore.org/"
+              className="font-bold hover:underline"
+            >
+              The Sparks Foundation
+            </Link>
+            , focusing on UI/UX and front-end development. Built interactive web
+            pages, enhanced accessibility, and improved overall design
+            responsiveness for better user engagement.
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/pro/hero-sections.png"
+              src="https://res.cloudinary.com/dwtcjjxwc/image/upload/v1743588060/portfolio/sparks_x4ufzp.png"
               alt="hero template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
@@ -82,8 +66,14 @@ const Data = () => {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-normal mb-4">
-            Web Development and Designing Intern at TECH-A-INTERN, enhancing
-            skills in front-end and responsive web design. Developed
+            Web Development and Designing Intern at{" "}
+            <Link
+              href="https://techaintern.com/"
+              className="font-bold hover:underline"
+            >
+              TECH-A-INTERN
+            </Link>
+            , enhancing skills in front-end and responsive web design. Developed
             user-friendly interfaces, optimized website performance, and
             implemented responsive design for various devices.
           </p>
@@ -101,29 +91,8 @@ const Data = () => {
           </div>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="/assests/slide-12.png"
+              src="https://res.cloudinary.com/dwtcjjxwc/image/upload/v1743588056/portfolio/techaintern_pmfqx4.png"
               alt="hero template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="/assests/punjabi.jpg"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
@@ -137,10 +106,17 @@ const Data = () => {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-normal mb-4">
-            Full Stack Developer Intern at Bharat Intern, working on back-end
-            and database management using MERN stack technologies. Created
-            RESTful APIs, managed cloud-based databases, and integrated
-            front-end and back-end components for seamless functionality.
+            Full Stack Developer Intern at {" "}
+            <Link
+              href="https://www.linkedin.com/company/bharat-intern/"
+              className="font-bold hover:underline"
+            >
+              Bharat Intern
+            </Link>{" "}
+            working on back-end and database management using MERN stack
+            technologies. Created RESTful APIs, managed cloud-based databases,
+            and integrated front-end and back-end components for seamless
+            functionality.
           </p>
           <div className="mb-8">
             <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-sm md:text-lg">
@@ -158,27 +134,6 @@ const Data = () => {
             <Image
               src="/assests/slide-12.png"
               alt="hero template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="/assests/punjabi.jpg"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
